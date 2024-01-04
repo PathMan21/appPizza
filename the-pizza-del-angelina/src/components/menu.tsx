724ad0f991b7eca3023fc091769709e21650bd98
unknown <mlafosse@lilyfacilitelavie.com>
2024-01-04 16:06:05 +0100

    ajout d'un menu pour le téléphone, et routeur fonctionnel

--- the-pizza-del-angelina/src/components/menu.tsx
+++ the-pizza-del-angelina/src/components/menu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Page1 from '../pages/page1';
 import Page2 from '../pages/page2';
+
 import {
   IonButtons,
   IonContent,
@@ -10,31 +11,37 @@ import {
   IonMenuButton,
   IonPage,
   IonTitle,
-  IonToolbar,
+  IonToolbar
 } from '@ionic/react';
-import "./menu.css"
+import "./menu.css";
 
 function Menu() {
   return (
     <Router>
-      <IonPage>
+      <IonMenu contentId="main-content">
         <IonHeader>
           <IonToolbar>
-            <IonTitle className="titreToolbar" slot="start"><h2>The Pizza Del Angelina !</h2></IonTitle>
-              <IonMenuButton />
-            <IonButtons>
-              <Link className="menuItem" to="/pages/page1"><img src="https://static.vecteezy.com/ti/vecteur-libre/p3/21028590-piece-de-pizza-noir-et-blanc-vecteur-ligne-illustration-vectoriel.jpg"></img>Carte</Link>
-              <Link className="menuItem" to="/pages/page2"><img src="https://static.vecteezy.com/ti/vecteur-libre/p3/21028590-piece-de-pizza-noir-et-blanc-vecteur-ligne-illustration-vectoriel.jpg"></img>Page 2</Link>
-            </IonButtons>
+          <IonTitle className="titreToolbar" slot="start"><h2>Menu</h2></IonTitle>
           </IonToolbar>
         </IonHeader>
-
         <IonContent>
-          <Switch>
-            <Route exact path="/pages/page1" component={Page1} />
-            <Route path="/pages/page2" component={Page2} />
-          </Switch>
+        <Link className="menuItem" to="/pages/page1"><img src="https://static.vecteezy.com/ti/vecteur-libre/p3/21028590-piece-de-pizza-noir-et-blanc-vecteur-ligne-illustration-vectoriel.jpg"></img>Carte</Link><br></br>
+        <Link className="menuItem" to="/pages/page2"><img src="https://static.vecteezy.com/ti/vecteur-libre/p3/21028590-piece-de-pizza-noir-et-blanc-vecteur-ligne-illustration-vectoriel.jpg"></img>Page 2</Link>
         </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle className="titreToolbar" slot="start"><h2>The Pizza Del Angelina !</h2></IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <Switch>
+          <Route exact path="/pages/page1" component={Page1}/>
+          <Route exact path="/pages/page2" component={Page2} />
+        </Switch>
       </IonPage>
     </Router>
   );
