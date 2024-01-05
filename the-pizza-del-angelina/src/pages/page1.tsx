f2ad9736c41ecca89151ce94c2ef469eda1646fd
unknown <mlafosse@lilyfacilitelavie.com>
2024-01-05 19:52:50 +0100

    j'ai fais fonctionné l'api du prof

--- the-pizza-del-angelina/src/pages/page1.tsx
+++ the-pizza-del-angelina/src/pages/page1.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   IonContent,
   IonHeader,
@@ -8,59 +8,58 @@ import {
   IonButton
 } from '@ionic/react';
 import { IonCol, IonGrid, IonRow } from '@ionic/react';
-import './page.css'
+import './page.css';
 
-
+interface Pizza {
+  id: number;
+  title: string;
+  image: string;
+  prix: string;
+  ingredients: string;
+}
 
 const Page1: React.FC = () => {
+  const [pizza, setPizza] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/pizzas");
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const jsonData = await response.json();
+        setPizza(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      
+    };
+
+    fetchData(); // Appel de la fonction fetchData() à l'intérieur de useEffect
+
+  }, []); // Tableau de dépendances vide pour un effet se produisant une seule fois au montage
+
   return (
     <IonPage>
       <IonHeader>
       </IonHeader>
       <IonContent className="ion-padding">
-      <IonGrid className='containerPizza'>
-        <IonRow>
-          <IonCol className='itemPizza'>
-            <img src="https://www.dominos.fr/ManagedAssets/FR/product/PCNR/FR_PCNR_fr_menu_12472.png?v-1736087109"></img>
-            <h2>Pizza Master Bacon-Fondu</h2>
-            <p>Une pizza faite pour les champions, double bacon, double bonheur !</p>
-            <button>Commander</button>
+        <IonGrid className='containerPizza'>
+          <IonRow>
+            {/* Mapping des données pizza et affichage */}
+            {pizza.map((entree: Pizza, index: number) => (
+            <IonCol key={index} className='itemPizza'>
+              <img src={entree.image}></img>
+              <h2>{entree.title}</h2>
+              <p>{entree.ingredients}</p>
+              <p>{entree.prix}</p>
+              <button>Commander</button>
             </IonCol>
-            
-          <IonCol className='itemPizza'><img src="https://www.dominos.fr/ManagedAssets/FR/product/PCAN/FR_PCAN_fr_menu_12192.png?v654698321"></img>
-            <h2>Pizza Chorizo-double dose</h2>
-            <p>Les accros de la viande seront ravis ! Chorizo + poivrons + fromage = bonheur</p>
-            <button>Commander</button>
-            </IonCol>
-
-          <IonCol className='itemPizza'><img src="https://www.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_menu_12192.png?v-713404695"></img>
-            <h2>Champignon Lovers</h2>
-            <p>Fromage et champignon, qui n'aimerait pas ?</p>
-            <button>Commander</button>
-            </IonCol>
-
-            <IonCol className='itemPizza'><img src="https://www.dominos.fr/ManagedAssets/FR/product/PBCG/FR_PBCG_fr_menu_12192.png?v25586971"></img>
-            <h2>Ketchup Mania</h2>
-            <p>Ketchup fromage et du jambon, la base quoi !</p>
-            <button>Commander</button>
-            </IonCol>
-
-            <IonCol className='itemPizza'><img src="https://www.dominos.fr/ManagedAssets/FR/product/PCMI/FR_PCMI_all_menu_12192.png?v-1467135987"></img>
-            <h2>Pizza Fromage Lover : 4 fromages</h2>
-            <p>4 fromages pour 4 fois plus de fondant</p>
-            <button>Commander</button>
-            </IonCol>
-
-
-            <IonCol className='itemPizza'><img src="https://www.dominos.fr/ManagedAssets/FR/product/PSVY/FR_PSVY_fr_menu_12192.png?v-367335066"></img>
-            <h2>Patate et lardons : pour les gourmands</h2>
-            <p>Lardon et pomme de terres, pour les gourmands</p>
-            <button>Commander</button>
-            </IonCol>
-
-            
-        </IonRow>
-      </IonGrid>
+            ))}
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
